43f4ebb6df0da7ce31e4823d194542a7e3d2cb15
Garrett Stot <garrettstott@gmail.com>
2016-03-20 19:30:48 -0600

    I think everything is working

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,22 +1,47 @@
 $(document).ready(function() {
-  var baseUrl = 'http://devpoint-ajax-example-server.herokuapp.com/api/v1/products';
+  var baseUrl = 'http://devpoint-ajax-example-server.herokuapp.com/api/v1/products/';
 
-
-
-  if(location.pathname === '/') {
-
-    $('.home_button').on('click', function() {
+    $(document).on('click', '.home_page_button', function(e) {
+      e.preventDefault();
       $('#products').removeClass('hidden');
-      $('#show_prodct').removeClass('hidden');
+      $('#products').addClass('hidden');
+      $('#show_product').removeClass('hidden');
       $('#show_product').addClass('hidden');
       $('#form_div').removeClass('hidden');
       $('#form_div').addClass('hidden');
-      getProducts();
+      $('#home_page').removeClass('hidden');
+    });
+
+    $(document).on('click', '.shop_now_button', function(e) {
+      e.preventDefault();
+      $('#product_name').html('');
+      $('#product_description').html('');
+      $('#product_price').html('');
+      $('#products').removeClass('hidden');
+      $('#show_product').removeClass('hiddnen');
+      $('#show_product').addClass('hidden');
+      $('#form_div').removeClass('hidden');
+      $('#form_div').addClass('hidden');
+      $('#home_page').removeClass('hidden');
+      $('#home_page').addClass('hidden');
+    });
+
+    $(document).on('click', '.home_button', function(e) {
+      e.preventDefault();
+      $('#product_name').html('');
+      $('#product_description').html('');
+      $('#product_price').html('');
+      $('#products').removeClass('hidden');
+      $('#show_product').removeClass('hiddnen');
+      $('#show_product').addClass('hidden');
+      $('#form_div').removeClass('hidden');
+      $('#form_div').addClass('hidden');
+      $('#home_page').removeClass('hidden');
+      $('#home_page').addClass('hidden');
     });
 
     function getProducts(data) {
       $('#products').html('');
-      $('#show_prouct').html('');
       $.ajax({
         url: baseUrl,
         type: 'GET',
@@ -51,10 +76,9 @@ $(document).ready(function() {
     $(document).on('click', '#buy', function(e) {
       e.preventDefault();
       var showProduct = $('#show_product');
-      
       $.ajax({
         type: 'GET',
-        url: baseUrl + '/' + this.dataset.id,
+        url: baseUrl + this.dataset.id,
         success: function(data){
           var product = data.product;
           var description = product.description ? product.description : 'Coming Soon';
@@ -63,6 +87,7 @@ $(document).ready(function() {
           $('#product_description').append(description);
           $('#product_price').append('$' + price);
           $('#delete').attr('data-id', product.id);
+          $('#edit').attr('data-id', product.id);
           showProduct.removeClass('hidden');
           $('#products').addClass('hidden');
         },
@@ -82,12 +107,14 @@ $(document).ready(function() {
     });
 
     $(document).on('click', '#delete', function() {
-      
       $.ajax({
-        url: baseUrl + '/' + this.dataset.id,
+        url: baseUrl + this.dataset.id,
         type: 'DELETE',
         success: function() {
-          alert('Deleted');
+          confirm('Are you sure?');
+          $('#product_name').html('');
+          $('#product_description').html('');
+          $('#product_price').html('');
           $('#show_product').addClass('hidden');
           $('#products').removeClass('hidden');
           $('#products').html('');
@@ -101,35 +128,63 @@ $(document).ready(function() {
 
     $(document).on('click', '#new_product', function(e){
       e.preventDefault();
+      var form = $('#new_product_form');
+      form.find('#form_name').html('New Product');
+      form.find("#product_name").val('');
+      form.find('#product_description').val('');
+      form.find('#product_price').val('');
+      $('#product_price').val('');
       $('#products').addClass('hidden');
+      $('#show_product').addClass('hidden');
       $('#form_div').removeClass('hidden');
     });
       
     $('#new_product_form').on('submit', function(e){
       e.preventDefault();
       $.ajax({
-        url: baseUrl + '/',
+        url: baseUrl,
         type: 'POST',
         dataType: 'JSON',
         data: $(this).serializeArray(),
         successs: function(data) {
-          $('#form_div').addClass('hidden');
-          $('#products').removeClass('hidden');
-          $('#products').html('');
           location.href = '/'
         },
         error: function(error) {
           console.log(error);
         }
       });
+        newForm = $('#new_product_form');
+        newForm.find('#product_name').val('');
+        newForm.find('#product_description').val('');
+        newForm.find('#product_name').val('');
+        $('#form_div').addClass('hidden');
+        $('#products').removeClass('hidden');
       getProducts();
+    });
+
+    $('#edit').on('click', function() {
+      $.ajax({
+        url: baseUrl + this.dataset.id,
+        type: 'GET',
+        success: function(data) {
+          $('#show_product').addClass('hidden');
+          $('#form_div').removeClass('hidden');
+          var product = data.product;
+          var editForm = $('#new_product_form');
+          editForm.find('#form_name').html("Edit Product");
+          editForm.find('#product_name').val(product.name);
+          editForm.find('#product_description').val(product.description);
+          editForm.find('#product_price').val(product.base_price);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
     });
 
 
 
-
     getProducts();
-  }
 
 
   
